fd1465af5aefe8f174b83ca6f22a4effb2f6329e
ericlb <eric.lirber@gmail.com>
2021-08-29 22:51:52 -0500

    Crea conexion a base de datos.

---
+++ src/keys.js
@@ -0,0 +1,8 @@
+module.exports = {
+    database: {
+        host: 'br4c2byotp2hfms7ymnj-mysql.services.clever-cloud.com',
+        user: 'ukifddvlay34y7xf',
+        password:'acsi7oMJvfiNGkJdlUGf',
+        database: 'br4c2byotp2hfms7ymnj'
+    }
+};
