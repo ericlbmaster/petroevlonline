d61416e598e8ac6a6b4c62c028c98744b9c9a407
ericlb <eric.lirber@gmail.com>
2021-08-30 23:16:24 -0500

    Implementa edicion de links.

--- src/routes/links.js
+++ src/routes/links.js
@@ -28,4 +28,22 @@ router.get('/delete/:id', async(req,res) => {
     res.redirect('/links');
 });
 
+router.get('/edit/:id', async(req,res)=>{
+    const {id} = req.params;
+    const links = await pool.query('SELECT * FROM links WHERE id= ?',[id]);
+    res.render('links/edit', {link: links[0]});
+});
+
+router.post('/edit/:id', async (req,res)=>{
+    const {id} = req.params;
+    const {title, description, url} = req.body;
+    const updatedLink = {
+        title,
+        description,
+        url
+    };
+    pool.query('UPDATE links SET ? WHERE id = ?', [updatedLink, id]);
+    res.redirect('/links');
+});
+
 module.exports = router;
