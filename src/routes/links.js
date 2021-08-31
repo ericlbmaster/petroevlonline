e1a286e3e5cfb601c0c83f62a255d40cf5d86b6a
ericlb <eric.lirber@gmail.com>
2021-08-31 11:20:32 -0500

    Implementa vista de profile, agrega proteccion de rutas con metodos isLoggedIn, isNotLoggedIn.

--- src/routes/links.js
+++ src/routes/links.js
@@ -3,11 +3,13 @@ const router = express.Router();
 
 const pool = require('../database');
 
-router.get('/add',(req,res)=>{
+const {isLoggedIn} = require('../lib/auth');
+
+router.get('/add', isLoggedIn, (req,res)=>{
     res.render('links/add');
 });
 
-router.post('/add', async (req,res) => {
+router.post('/add', isLoggedIn, async (req,res) => {
     const {title, url, description} = req.body;
     const created_at = new Date();
     const newLink = {
@@ -18,25 +20,25 @@ router.post('/add', async (req,res) => {
     res.redirect('/links');
 });
 
-router.get('/', async (req,res) => {
+router.get('/', isLoggedIn, async (req,res) => {
      const links = await pool.query('SELECT * FROM links');
      res.render('links/list',{links});
 });
 
-router.get('/delete/:id', async(req,res) => {
+router.get('/delete/:id', isLoggedIn, async(req,res) => {
     const {id} = req.params;
     await pool.query('DELETE FROM links WHERE id = ?',[id]);
     req.flash('success','Link deleted successfully');
     res.redirect('/links');
 });
 
-router.get('/edit/:id', async(req,res)=>{
+router.get('/edit/:id', isLoggedIn, async(req,res)=>{
     const {id} = req.params;
     const links = await pool.query('SELECT * FROM links WHERE id= ?',[id]);
     res.render('links/edit', {link: links[0]});
 });
 
-router.post('/edit/:id', async (req,res)=>{
+router.post('/edit/:id', isLoggedIn, async (req,res)=>{
     const {id} = req.params;
     const {title, description, url} = req.body;
     const updatedLink = {
