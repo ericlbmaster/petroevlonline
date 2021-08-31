f3ae9020054886f7dc4ddcca5f0d22633904934a
ericlb <eric.lirber@gmail.com>
2021-08-31 10:45:57 -0500

    Implementa sign in, agrega rutas y mensajes para inicio de sesion correcto, incorrecto e invalido.

--- src/lib/passport.js
+++ src/lib/passport.js
@@ -4,6 +4,30 @@ const LocalStrategy = require('passport-local').Strategy;
 const pool = require('../database');
 const helpers = require('../lib/helpers')
 
+passport.use('local.signin', new LocalStrategy({
+    usernameField: 'username',
+    passwordField: 'password',
+    passReqToCallback: true
+ }, async(req, username, password, done) =>{
+     try{
+        const rows = await pool.query('SELECT * FROM users WHERE username = ?', [username]);
+        if(rows.length > 0){
+            const user = rows[0];
+            const validPassword = await helpers.matchPassword(password, user.password);
+            if(validPassword){
+                done(null, user, req.flash('success', 'Welcome '+ user.username));
+            }
+            else{
+                done(null, false, req.flash('message','Invalid user or password'));
+            }
+        } else{
+            return done(null, false, req.flash('message','Please check the login data'));
+        }
+    } catch(e){
+        console.log(e);
+    }
+ }));
+
 passport.use('local.signup', new LocalStrategy({
     usernameField: 'username',
     passwordField: 'password',
